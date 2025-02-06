6ba2b767cf3d1d3a993397e571d0da22f664ee74
action <8768611@qq.com>
2025-02-07 00:20:35 +0800

    20

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,15 @@ yarn build
 # 进入生成的文件夹
 cd .vuepress/dist
 
+git config --global user.name "action"
+git config --global user.email "8768611@qq.com"
 git init
 git add -A
 git commit -m 'deploy'
 git checkout -b gh-pages
 
 # 如果发布到 https://<USElsRNAME>.github.io/<REPO>
-git push -f https://github.com/kidoneself/kidoneself.github.io.git main:gh-pages
+git push -f https://github.com/kidoneself/kidoneself.github.io.git master:gh-pages
 
 
 cd -
